4d4212db034aef8a19c861fdc9b9c721a494c5aa
Nunny R <nunnyreyes@gmail.com>
2021-06-01 13:10:13 -0400

    try and catching

--- book-club/src/App.jsx
+++ book-club/src/App.jsx
@@ -10,25 +10,26 @@ const App = () => {
   //the DOM.
 
   console.log("this message is going to load everytime the component renders")
+  
   useEffect(() => {
-    const fetchData = () => {
-      fetch("https://book-club-json.herokuapp.com/books")
-      .then((res) => {
-        console.log("what we got back:", res)
-        //grab our json res and turn it into a js object
-        return res.json()
-      })
-      .then((books) => {
-        console.log("here is after", books)
-        return setBooks(books)
+    const fetchData = async () => {
 
-      })
+      try {
+      const res = await fetch("https://book-club-jason.herokuapp.com/books")
+      console.log("what we got back:", res)
 
+     
+        const books = await res.json()
+        console.log("our json-ified res", books)
+        setBooks(books)
+      } catch (erros) {
+        console.log(errors)
+      }
     }
-
     fetchData()
-
   }, [])
+
+  console.log("the books array in our state", books)
   //if we dont have an empty array dependency, it will get
   //stuck in an infinite loop
 
